81e1dcccec0f58d6f91181276381bdabae8019ee
unknown <jair.lopes@fatec.sp.gov.br>
2022-06-15 19:03:59 -0300

    Básico sobre Função

---
+++ 02 - Funções/Aula15 - Básico sobre funções/exemplo01.js
@@ -0,0 +1,24 @@
+let a = 4;
+console.log(a);
+
+
+// Function Declaration
+function bomDia() {
+    console.log('Bom dia!');
+}
+
+bomDia();
+
+// Function Expression
+const boaTarde = function() {
+    console.log('Boa tarde!');
+}
+
+boaTarde();
+
+function somar(a, b) {
+    return;
+}
+
+const resultado = somar(3, 4, 5, 6, 7, 8);
+console.log(resultado);
