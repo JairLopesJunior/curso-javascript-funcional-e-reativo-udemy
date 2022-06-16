cb8e6a45f76419454f5e44d827a5d377e80c3aa7
unknown <jair.lopes@fatec.sp.gov.br>
2022-06-16 12:07:00 -0300

    Função Map 02

---
+++ 02 - Funções/Aula22 - Função Map 02/exemplo.js
@@ -0,0 +1,7 @@
+const nums = [1, 2, 3, 4, 5];
+const dobro = n => n * 2;
+console.log(nums.map(dobro));
+
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa'];
+const primeiraLetra = texto => texto[0];
+console.log(nomes.map(primeiraLetra));
