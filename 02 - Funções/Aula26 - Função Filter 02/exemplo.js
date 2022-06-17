c1592929adec8b1a7b76b9930fbdba53d91be480
unknown <jair.lopes@fatec.sp.gov.br>
2022-06-17 17:17:13 -0300

    Função Filter 02

---
+++ 02 - Funções/Aula26 - Função Filter 02/exemplo.js
@@ -0,0 +1,17 @@
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.99 },
+    { nome: 'Impressora', qtde: 0, preco: 649.50 },
+    { nome: 'Caderno', qtde: 4, preco: 27.10 },
+    { nome: 'Lapis', qtde: 3, preco: 5.82 },
+    { nome: 'Tesouro', qtde: 1, preco: 19.20 }
+];
+
+
+const qtdeMaiorQueZero = item => item.qtde > 0;
+const getNome = item => item.nome;
+
+const itensValidos = carrinho
+    .filter(qtdeMaiorQueZero)
+    .map(getNome);
+
+console.log(itensValidos);
