fb625f701d9b1a9a636660d71cbac649056d5e62
unknown <jair.lopes@fatec.sp.gov.br>
2022-06-22 21:48:05 -0300

    Async/Await 01

---
+++ 02 - Funções/Aula40 - Async Await 01/exemplo.js
@@ -0,0 +1,29 @@
+function esperarPor(tempo = 2000) {
+    return new Promise(function(resolve) {
+        setTimeout(() => resolve(), tempo);
+    }) 
+}
+
+function retornarValor() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 5000);
+    })
+}
+
+async function executar() {
+    let valor = await retornarValor();
+
+    await esperarPor(1500);
+    console.log(`Async/Await ${valor}...`);
+
+    await esperarPor(1500);
+    console.log(`Async/Await ${valor + 1}...`);
+
+    await esperarPor(1500);
+    console.log(`Async/Await ${valor + 2}...`);
+
+    return valor + 3;
+}
+
+executar()
+    .then(console.log);
