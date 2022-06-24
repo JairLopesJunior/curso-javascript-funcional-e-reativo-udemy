7dde1b830ecd42726d31a27749ba92794e2b8c26
unknown <jair.lopes@fatec.sp.gov.br>
2022-06-23 21:14:58 -0300

    OO 03

---
+++ 02 - Funções/Aula44 - OO 03/exemplo.js
@@ -0,0 +1,41 @@
+function Produto(nome, preco, desc = 0.15) {
+    this.nome = nome;
+    this.preco = preco;
+    this._desc = desc;
+
+    this.precoFinal = function() {
+        return this.preco * (1 - this._desc);
+    }
+}
+
+Produto.prototype.log = function() {
+    console.log(`Nome: ${this.nome} Preço: R$ ${this.preco}`);
+}
+
+const p1 = new Produto('Caneta', 10);
+console.log(p1.nome);
+p1.log();
+
+Object.defineProperty(Produto.prototype, 'desc', {
+    get: function() {
+        return this._desc;
+    },
+    set: function(novoDesc) {
+        if(novoDesc >= 0 && novoDesc <= 1) {
+            this._desc = novoDesc;
+        }
+    }
+});
+
+Object.defineProperty(Produto.prototype, 'descString', {
+    get: function() {
+        return `${this._desc * 100}% de desconto`;
+    }
+});
+
+const p2 = new Produto('Geladeira', 3000);
+console.log(p2.preco);
+console.log(p2.precoFinal());
+p2.desc = 0.99;
+console.log(p2.desc);
+console.log(p2.descString);
