a25495be3fc6a00f6d6954529998657e360e9998
unknown <jair.lopes@fatec.sp.gov.br>
2022-06-27 23:51:57 -0300

    Separando Palavras

--- 03 - Projeto 01 - Palavras Mais Usadas/index.js
+++ 03 - Projeto 01 - Palavras Mais Usadas/index.js
@@ -9,13 +9,24 @@ const simbolos = [
     '(', ')'
 ];
 
+const mesclarElementos = conteudos => conteudos.join(' ');
+const separarPorLinhas = todoConteudo => todoConteudo.split('\n');
+const separarPorPalavras = todoConteudo => todoConteudo.split(' ');
+
+function agruparPalavras() {
+    
+}
+
 fn.lerDiretorio(caminho)
     .then(arquivos => fn.elementoTerminadosCom(arquivos, '.srt'))
     .then(fn.lerArquivos)
-    .then(conteudos => conteudos.join('\n'))
-    .then(todoConteudo => todoConteudo.split('\n'))
+    .then(mesclarElementos)
+    .then(separarPorLinhas)
     .then(fn.removerElementosSeVazio)
     .then(fn.removerElementosSeIncluir('-->'))
     .then(fn.removerElementosSeApenasNumero)
     .then(fn.removerSimbolos(simbolos))
+    .then(mesclarElementos)
+    .then(separarPorPalavras)
+    .then(fn.removerElementosSeVazio)
     .then(console.log);
