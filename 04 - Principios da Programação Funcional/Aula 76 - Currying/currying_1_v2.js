ca8e8bb3627e58ef48cb29d389b55a5ad66d40c5
unknown <jair.lopes@fatec.sp.gov.br>
2022-06-29 10:21:46 -0300

    Currying

---
+++ 04 - Principios da Programação Funcional/Aula 76 - Currying/currying_1_v2.js
@@ -0,0 +1,21 @@
+function textoComTamanhoEntre(min) {
+    return function(max) {
+        return function(erro) {
+            return function(texto) {
+                // Lazy Evaluation
+                const tamanho = (texto || '').trim().length;
+
+                if(tamanho < min || tamanho > max) {
+                    throw erro;
+                }
+            }
+        }
+    }
+}
+
+const forcarTamanhoPadrao = textoComTamanhoEntre(4)(255);
+const forcarNomeProdutoValido = forcarTamanhoPadrao('Nome produto inválido!');
+
+const p1 = { nome: 'A', preco: 14.99, desc: 0.25 };
+forcarTamanhoPadrao('Nome inválido!')(p1.nome);
+forcarNomeProdutoValido(p1.nome);
