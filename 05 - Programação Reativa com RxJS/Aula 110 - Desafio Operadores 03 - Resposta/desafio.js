82037ca74a0b0ebac3f4ea074fa74f9c8fc1a1f1
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-02 13:23:53 -0300

    Desafio Operadores 03 - Resposta 01

---
+++ 05 - Programação Reativa com RxJS/Aula 110 - Desafio Operadores 03 - Resposta/desafio.js
@@ -0,0 +1,44 @@
+const { from, Observable } = require('rxjs');
+
+function createPipeableOperator(nextFn) {
+    return function(source) {
+        return Observable.create(subscriber => {
+            source.subscribe({
+                next(v) {
+                    nextFn(subscriber, v);
+                }
+            });
+        });
+    }
+}
+
+function primeiro() {
+    return createPipeableOperator((subscriber, v) => {
+        subscriber.next(v);
+        subscriber.complete();
+    });
+}
+
+function ultimo() {
+    return function(source) {
+        return Observable.create(subscriber => {
+            let ultimo;
+            source.subscribe({
+                next(v) {
+                    ultimo = v;
+                },
+                complete() {
+                    subscriber.next(ultimo);
+                    subscriber.complete();
+                }
+            });
+        });
+    }
+}
+
+from([1, 2, 3, 4, 5])
+    .pipe(
+        primeiro(),
+        //ultimo()
+    )
+    .subscribe(console.log);
