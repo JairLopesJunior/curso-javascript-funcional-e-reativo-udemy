236148ea01cf48337531067e7da192b16414fd4c
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-03 19:09:23 -0300

    Operador MergeMap

---
+++ 06 - Projto 02 - Migrando Código para RxJS/Aula 127 - Operador MergeMap/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fn = require('./funcoes');
+const _ = require('lodash');
+const { toArray, map } = require('rxjs/operators');
+
+const caminho = path.join(__dirname, 'legendas');
+
+const simbolos = [
+    '.', '?', '-', ',', '"', '♪',
+    '_', '<i>', '</i>', '\r', '[', ']',
+    '(', ')', '!'
+];
+
+fn.lerDiretorio(caminho)
+    .pipe(
+        fn.elementoTerminadosCom('.srt'),
+        fn.lerArquivo(),
+        fn.separarTextoPor('\n'),
+        fn.removerElementosSeVazio(),
+        fn.removerElementosSeApenasNumero(),
+        fn.removerSimbolos(simbolos),
+        fn.separarTextoPor(' '),
+        fn.removerElementosSeVazio(),
+        fn.removerElementosSeApenasNumero(),
+        toArray(),
+        fn.agruparElementos(),
+        map(array => _.sortBy(array, el => -el.qtde))
+    )
+    .subscribe(console.log);
