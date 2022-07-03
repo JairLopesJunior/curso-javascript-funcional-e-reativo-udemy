23a8a606bbb6c375bec5b692b90761e09d79bedc
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-03 15:18:11 -0300

    Remover Símbolos

---
+++ 06 - Projto 02 - Migrando Código para RxJS/Aula 124 - Remover Símbolos/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fn = require('./funcoes');
+
+const caminho = path.join(__dirname, 'legendas');
+
+const simbolos = [
+    '.', '?', '-', ',', '"', '♪',
+    '_', '<i>', '</i>', '\r', '[', ']',
+    '(', ')', '!'
+];
+
+fn.lerDiretorio(caminho)
+    .pipe(
+        fn.elementoTerminadosCom('.srt'),
+        fn.lerArquivo(),
+        fn.separarTextoPor('\n'),
+        fn.removerElementosSeVazio(),
+        fn.removerElementosSeApenasNumero(),
+        fn.removerSimbolos(simbolos),
+        fn.separarTextoPor(' '),
+        fn.removerElementosSeVazio(),
+        fn.removerElementosSeApenasNumero(),
+    )
+    .subscribe(console.log);
