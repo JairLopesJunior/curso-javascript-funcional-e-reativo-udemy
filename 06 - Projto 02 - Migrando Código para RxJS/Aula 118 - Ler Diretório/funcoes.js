5dc9fc7d3eb4f0f1792cd9a342ddaf00f891c0df
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-03 11:52:06 -0300

    Ler Diretório

---
+++ 06 - Projto 02 - Migrando Código para RxJS/Aula 118 - Ler Diretório/funcoes.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const path = require('path');
+const { Observable } = require('rxjs');
+
+function lerDiretorio(caminho) {
+    return new Observable(subscriber => {
+        try {
+            fs.readdirSync(caminho).forEach(arquivo => {
+                subscriber.next(path.join(caminho, arquivo));
+            })
+            subscriber.complete();
+        } catch(e) {
+            subscriber.error(e);
+        }
+    });
+}
+
+function lerArquivo(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' });
+            resolve(conteudo.toString());
+        } catch(e) {
+            reject(e);
+        }
+    })
+}
+
+function lerArquivos(caminhos) {
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)));
+}
+
+function elementoTerminadosCom(array, padraoTextual) {
+    return array.filter(el => el.endsWith(padraoTextual));
+}
+
+function removerElementosSeVazio(array) {
+    return array.filter(el => el.trim());
+}
+
+function removerElementosSeIncluir(padraoTextual) {
+    return function(array) {
+        return array.filter(el => !el.includes(padraoTextual));
+    }
+}
+
+function removerElementosSeApenasNumero(array) {
+    return array.filter(el => {
+        const num = parseInt(el.trim());
+        return num!== num;
+    });
+}
+
+function removerSimbolos(simbolos) {
+    return function(array) {
+        return array.map(el => {
+            return simbolos.reduce((acc, simbolo) => {
+                return acc.split(simbolo).join('');
+            }, el)
+        });
+    }
+}
+
+function ordernarPorAtribNumerico(attr, ordem = 'asc') {
+    return function(array) {
+        const asc = (o1, o2) => o1[attr] - o2[attr];
+        const desc = (o1, o2) => o2[attr] - o1[attr];
+        return array.sort(ordem === 'asc' ? asc : desc);
+    }
+}
+
+module.exports = {
+    lerDiretorio,
+    lerArquivo,
+    lerArquivos,
+    elementoTerminadosCom,
+    removerElementosSeVazio,
+    removerElementosSeIncluir,
+    removerElementosSeApenasNumero,
+    removerSimbolos,
+    ordernarPorAtribNumerico
+};
