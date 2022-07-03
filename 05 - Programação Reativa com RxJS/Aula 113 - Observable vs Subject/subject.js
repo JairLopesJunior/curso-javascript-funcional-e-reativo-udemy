22b5f89fe5b40368cb1044f4d0e420e6565f2510
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-02 22:26:00 -0300

    Observable vs Subject

---
+++ 05 - Programação Reativa com RxJS/Aula 113 - Observable vs Subject/subject.js
@@ -0,0 +1,30 @@
+const { Observable, Subject } = require('rxjs');
+
+function getObservable() {
+    return new Observable(subscriber => {
+        setTimeout(() => {
+            console.log('#1 Obs...');
+            subscriber.next(Math.random());
+            subscriber.complete();
+        }, 1000)
+    });
+}
+
+const obs = getObservable();
+obs.subscribe(console.log);
+obs.subscribe(console.log);
+
+
+function getSubject() {
+    const sub = new Subject();
+    setTimeout(() => {
+        console.log('#2 Sub...');
+        sub.next(Math.random());
+        sub.complete();
+    }, 1000)
+    return sub;
+}
+
+const sub = getSubject();
+sub.subscribe(console.log);
+sub.subscribe(console.log);
